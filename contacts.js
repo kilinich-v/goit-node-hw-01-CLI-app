44200a7c13b2b876ab1110b0aebfc50bfbf38790
Vladimir Kilinich <kylynych.v@gmail.com>
2021-06-09 12:56:16 +0300

    remove comments

--- contacts.js
+++ contacts.js
@@ -1,71 +1,80 @@
-const fs = require("fs").promises
-const path = require("path")
-const shortid = require("shortid")
+const fs = require('fs').promises;
+const path = require('path');
+const shortid = require('shortid');
 
-const contactsPath = path.join("./db/contacts.json")
+const contactsPath = path.join('./db/contacts.json');
 
-// TODO: задокументировать каждую функцию
 function listContacts() {
-	return fs
-		.readFile(contactsPath)
-		.then(data => console.table(JSON.parse(data)))
-		.catch(err => console.log(err.message))
+  return fs
+    .readFile(contactsPath)
+    .then(data => console.table(JSON.parse(data)))
+    .catch(err => console.log(err.message));
 }
 
 function getContactById(contactId) {
-	return fs
-		.readFile(contactsPath)
-		.then(data => {
-			const contacts = JSON.parse(data)
+  return fs
+    .readFile(contactsPath)
+    .then(data => {
+      const contacts = JSON.parse(data);
 
-			return console.table(contacts.find(({ id }) => id === contactId))
-		})
-		.catch(err => console.log(err.message))
+      return console.table(contacts.find(({ id }) => id === contactId));
+    })
+    .catch(err => console.log(err.message));
 }
 
 function removeContact(contactId) {
-	return fs
-		.readFile(contactsPath)
-		.then(data => {
-			const contactsList = JSON.parse(data)
+  return fs
+    .readFile(contactsPath)
+    .then(data => {
+      const contactsList = JSON.parse(data);
 
-			const newContactsList = [...contactsList.filter(({ id }) => id !== contactId)]
+      const newContactsList = [
+        ...contactsList.filter(({ id }) => id !== contactId)
+      ];
 
-			fs.writeFile(contactsPath, JSON.stringify(newContactsList, null, "\t"), "utf8")
+      fs.writeFile(
+        contactsPath,
+        JSON.stringify(newContactsList, null, '\t'),
+        'utf8'
+      );
 
-			console.table(newContactsList)
-			return console.table(`Сontact remove`)
-		})
-		.catch(err => console.log(err.message))
+      console.table(newContactsList);
+      return console.table(`Сontact remove`);
+    })
+    .catch(err => console.log(err.message));
 }
 
 function addContact(name, email, phone) {
-	const newContact = {
-		id: shortid.generate(),
-		name,
-		email,
-		phone,
-	}
+  const newContact = {
+    id: shortid.generate(),
+    name,
+    email,
+    phone
+  };
 
-	return fs
-		.readFile(contactsPath)
-		.then(data => {
-			const contactsList = JSON.parse(data)
+  return fs
+    .readFile(contactsPath)
+    .then(data => {
+      const contactsList = JSON.parse(data);
 
-			const newContactsList = [...contactsList, newContact]
+      const newContactsList = [...contactsList, newContact];
 
-			fs.writeFile(contactsPath, JSON.stringify(newContactsList, null, "\t"), "utf8")
+      fs.writeFile(
+        contactsPath,
+        JSON.stringify(newContactsList, null, '\t'),
+        'utf8'
+      );
 
-			console.table(newContactsList)
-			return console.table(`Сontact added`)
-		})
-		.catch(err => console.log(err.message))
+      console.table(newContactsList);
+      return console.table(`Сontact added`);
+    })
+    .catch(err => console.log(err.message));
 }
 
 module.exports = {
-	contactsPath,
-	listContacts,
-	getContactById,
-	removeContact,
-	addContact,
-}
+  contactsPath,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+};
